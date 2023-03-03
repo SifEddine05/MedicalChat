611484b5cee643650ea417edc39041cfb8e5db08
SifEddine05 <sifousamir72@gmail.com>
2023-03-03 22:45:44 +0100

    add the logout

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,17 +1,48 @@
 import hospital from '../assets/hospital.png'
-import logout from '../assets/logout.png'
+import logout1 from '../assets/logout.png'
 import { ChannelList, useChatContext } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
 import TeamChannel from './TeamChannel';
 import TeamChannelPreview from './TeamChannelPreview';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import { useState } from 'react';
 
 
 const SideBar = () => {
     const client = StreamChat.getInstance("9m7fqeq4sq8h");
+    const navigate = useNavigate()
+    const cookies = new Cookies()
+
+    const [Err , setErr] = useState(true)
+    const [Msg ,setMsg]=useState('')
 
     const filters = { members: { $in: [client.userID] } };
     const teamChannel=(channels) => {
         return channels.filter((channel) => channel.type === 'team');
+    }
+
+    const logout =()=>{
+        setErr(false)
+        fetch('http://localhost:8000/logout')
+        .then((res)=>{
+            return res.json()
+        })
+        .then((data)=>{
+            if(data.success===true)
+            {
+                cookies.remove('token')
+                cookies.remove('userID')
+                cookies.remove('hasedpassword')
+                cookies.remove('userName')
+                cookies.remove('fullName')
+                navigate('/login')
+            }
+        })
+        .catch(err=>{
+            setErr(true)
+            setMsg(err)
+        })
     }
     
     return ( 
@@ -20,7 +51,7 @@ const SideBar = () => {
             <img src={hospital} alt="" className='mx-auto w-[30px] rounded-xl bg-white p-1' />
             
             <div className='mx-auto w-full flex justify-center'>
-                <button><img src={logout} alt="" className='  w-[30px] rounded-xl bg-white p-1 mt-3 hover:bg-red-500' /></button>
+                <button onClick={logout} ><img src={logout1} alt="" className='  w-[30px] rounded-xl bg-white p-1 mt-3 hover:bg-red-500' /></button>
             </div>
         </div>
         <div className='w-[80%] bg-[#005FFF] shadow-lg '>
@@ -31,7 +62,9 @@ const SideBar = () => {
                 List={(listProps)=>( <TeamChannel {...listProps} type="team" /> )}
                 Preview={(previewProps) => (<TeamChannelPreview {...previewProps} type="team" /> )}
             />
+            {Err && <h3 className=' text-center md:text-[16px] sm:text-[14px] text-[11px] font-bold text-red-600'>{Msg}</h3>}
         </div>
+
     </div> );
 }
  
--- client/src/components/TeamChannel.js
+++ client/src/components/TeamChannel.js
@@ -8,7 +8,7 @@ const TeamChannel = ({children}) => {
             <h3 className="text-[16px] text-white opacity-75">Channels</h3>
             <button><AddChannel   /></button>
         </div> 
-        <div className='text-[10px] w-[90%] flex flex-col items-center text-white justify-center mt-2'>
+        <div className='text-[10px] text-center w-[90%] flex p-2 px-6 flex-col items-center text-white justify-center mt-2'>
             {children}
         </div>
     </div>
